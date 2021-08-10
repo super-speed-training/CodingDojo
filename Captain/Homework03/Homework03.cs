f17cb2e88382dd99565c8852dcbf644973027b9d
Anawat <anawat@perfenterprise.com>
2021-08-10 17:45:23 +0700

    resolve homework03
    add project and unit test of homework03 with issue resolve code
    
    Fix #15

---
+++ Captain/Homework03/Homework03.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Homework03
+{
+    public class Homework03 : IHomework03
+    {
+        public IEnumerable<string> CapitalizedText(IEnumerable<string> text) => text.Select(it => it.ToUpper());
+    }
+}
