0311eabe085a6923e80b619a886856a04862d23e
earn <earndarkness@perfenterprise.com>
2021-08-10 18:57:31 +0700

    initialize project.

---
+++ HomeWork03/Homework03Console/Homework03Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework03Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
