185fc2e102b7ee077d05ea5f965ff9d9628df3e7
perawatt <perawatt@perfenterprise.com>
2021-08-11 10:40:11 +0700

    implement homework 03

---
+++ HomeWork03/CaptalizationText/Homework03.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CaptalizationText
+{
+    public class Homework03 : IHomework03
+    {
+        public IEnumerable<string> CapitalizedText(IEnumerable<string> text)
+        {
+            return text.Select(it => it.ToUpper());
+        }
+
+    }
+}
---
+++ HomeWork03/CaptalizationText/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaptalizationText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputSet = new List<string>();
+            var svc = new CaptalizationText.Homework03();
+            Console.WriteLine("Please enter words :");
+            while (true)
+            {
+                var words = Console.ReadLine().ToString();
+                if (words == string.Empty)
+                {
+                    break;
+                }
+                inputSet.Add(words);
+            }
+            var wordUpper = svc.CapitalizedText(inputSet);
+            Console.WriteLine("Result :");
+            Console.WriteLine(string.Join(Environment.NewLine, wordUpper));
+        }
+    }
+}
