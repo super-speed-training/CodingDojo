06180d9d708369ff71ddcbe540f6366ceb02e2d1
parada <parada@perfenterprise.com>
2021-08-12 14:05:40 +0700

    Implement class.

---
+++ HomeWork03/HomeWork03/Homework03.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork03
+{
+    public class Homework03 : IHomework03
+    {
+        public IEnumerable<string> CapitalizedText(IEnumerable<string> text)
+        {
+            return text.Select(it => it.ToUpper());
+        }
+    }
+}
---
+++ HomeWork03/HomeWork03/IHomework03.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork03
+{
+    public interface IHomework03
+    {
+        IEnumerable<string> CapitalizedText(IEnumerable<string> text);
+    }
+}
